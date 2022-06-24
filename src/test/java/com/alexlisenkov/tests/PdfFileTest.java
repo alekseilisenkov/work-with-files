4c91f87485bf891c3e3d20f62b4195cc08485edb
alekseilisenkov <alexlisenkov92@mail.ru>
2022-06-24 23:19:20 +0300

    add pdf file test

--- src/test/java/com/alexlisenkov/tests/PdfFileTest.java
+++ src/test/java/com/alexlisenkov/tests/PdfFileTest.java
@@ -1,4 +1,23 @@
 package com.alexlisenkov.tests;
 
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.InputStream;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PdfFileTest {
+    @Test
+    void downloadPdfTest() throws Exception {
+
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("pdf.pdf")) {
+            PDF parsed = new PDF(stream);
+            assertThat(parsed.text).contains("REWRITING GIT HISTORY");
+        }
+    }
 }
