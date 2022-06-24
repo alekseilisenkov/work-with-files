e22754d40569e79260f0bbd90941cf43526aa95c
alekseilisenkov <alexlisenkov92@mail.ru>
2022-06-24 12:46:15 +0300

    first commit

--- src/test/java/com/alexlisenkov/tests/TxtTest.java
+++ src/test/java/com/alexlisenkov/tests/TxtTest.java
@@ -2,6 +2,6 @@ package com.alexlisenkov.tests;
 
 public class TxtTest {
     public static void main(String[] args) {
-        System.out.println("12");
+        System.out.println("123");
     }
 }
