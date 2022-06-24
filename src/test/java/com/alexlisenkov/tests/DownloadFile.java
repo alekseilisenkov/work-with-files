85ee757c4cb3dd23d0ea07aa84f01f3b22574010
alekseilisenkov <alexlisenkov92@mail.ru>
2022-06-24 13:59:40 +0300

    test with download and read file

---
+++ src/test/java/com/alexlisenkov/tests/DownloadFile.java
@@ -0,0 +1,29 @@
+package com.alexlisenkov.tests;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DownloadFile {
+    @Test
+    void downloadFile() throws Exception {
+        Selenide.open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File downloadedFile = Selenide.$("#raw-url").download();
+        System.out.println(downloadedFile.toString());
+
+        String s;
+        try (InputStream is = new FileInputStream(downloadedFile)) {
+            s = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        }
+        System.out.println(s);
+
+        Assertions.assertTrue(s.contains("Gradle Enterprise"));
+    }
+}
--- src/test/java/com/alexlisenkov/tests/TxtTest.java
+++
@@ -1,7 +0,0 @@
-package com.alexlisenkov.tests;
-
-public class TxtTest {
-    public static void main(String[] args) {
-        System.out.println("123");
-    }
-}
